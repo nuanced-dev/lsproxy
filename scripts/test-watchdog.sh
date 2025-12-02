19f250d97498e0ac7990f29d6b91a9f517431483
Hendrik van Antwerpen <hendrik@nuanced.dev>
2025-12-02 15:56:22 +0100

    Fix logging

--- scripts/test-watchdog.sh
+++ scripts/test-watchdog.sh
@@ -192,7 +192,7 @@ docker run -d \
     -p 4455:4444 \
     -v /var/run/docker.sock:/var/run/docker.sock \
     -v "$WORKSPACE_PATH:/mnt/workspace" \
-    -e RUST_LOG=info \
+    -e RUST_LOG=info,nuanced_lsp_proxy=debug,proxy=debug,nuanced_lsp_wrapper=debug,wrapper=debug \
     -e USE_AUTH=false \
     -e WRAPPER_IMAGE=nuanced-lsp-wrapper:${RUST_VERSION} \
     -e WATCHDOG_IMAGE=nuanced-lsp-watchdog:${RUST_VERSION} \
@@ -262,7 +262,7 @@ docker run -d \
     -p 4456:4444 \
     -v /var/run/docker.sock:/var/run/docker.sock \
     -v "$WORKSPACE_PATH:/mnt/workspace" \
-    -e RUST_LOG=info \
+    -e RUST_LOG=info,nuanced_lsp_proxy=debug,proxy=debug,nuanced_lsp_wrapper=debug,wrapper=debug \
     -e USE_AUTH=false \
     -e WRAPPER_IMAGE=nuanced-lsp-wrapper:${RUST_VERSION} \
     -e WATCHDOG_IMAGE=nuanced-lsp-watchdog:${RUST_VERSION} \
@@ -313,7 +313,7 @@ docker run -d \
     -p 4457:4444 \
     -v /var/run/docker.sock:/var/run/docker.sock \
     -v "$WORKSPACE_PATH:/mnt/workspace" \
-    -e RUST_LOG=warn \
+    -e RUST_LOG=info,nuanced_lsp_proxy=debug,proxy=debug,nuanced_lsp_wrapper=debug,wrapper=debug \
     -e USE_AUTH=false \
     -e WRAPPER_IMAGE=nuanced-lsp-wrapper:${RUST_VERSION} \
     -e WATCHDOG_IMAGE=nuanced-lsp-watchdog:${RUST_VERSION} \
@@ -324,7 +324,7 @@ docker run -d \
     -p 4458:4444 \
     -v /var/run/docker.sock:/var/run/docker.sock \
     -v "$WORKSPACE_PATH:/mnt/workspace" \
-    -e RUST_LOG=warn \
+    -e RUST_LOG=info,nuanced_lsp_proxy=debug,proxy=debug,nuanced_lsp_wrapper=debug,wrapper=debug \
     -e USE_AUTH=false \
     -e WRAPPER_IMAGE=nuanced-lsp-wrapper:${RUST_VERSION} \
     -e WATCHDOG_IMAGE=nuanced-lsp-watchdog:${RUST_VERSION} \
