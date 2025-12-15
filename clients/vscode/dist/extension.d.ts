528b0adaf174c92b2eefff12e10af168dc331222
Hendrik van Antwerpen <hendrik@nuanced.dev>
2025-12-15 14:44:37 +0100

    Remove dist from repo

--- clients/vscode/dist/extension.d.ts
+++
@@ -1,4 +0,0 @@
-import * as vscode from "vscode";
-export declare function activate(context: vscode.ExtensionContext): void;
-export declare function deactivate(): Thenable<void> | undefined;
-//# sourceMappingURL=extension.d.ts.map
