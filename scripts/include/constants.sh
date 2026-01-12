8025e4348eb598f3471819d4b2e778920986d69e
Hendrik van Antwerpen <hendrik@nuanced.dev>
2026-01-12 18:14:35 +0100

    FIx variable name clash

--- scripts/include/constants.sh
+++ scripts/include/constants.sh
@@ -1,16 +1,16 @@
-SCRIPT_DIR="$(cd "$(dirname "$(realpath "${BASH_SOURCE[0]}")")" && pwd)"
+CONSTANTS_DIR="$(cd "$(dirname "$(realpath "${BASH_SOURCE[0]}")")" && pwd)"
 
 if [ -n "${LANGUAGE_IMAGE_VERSION:+x}" ]; then
     DEFAULT_LANGUAGE_TAG="$LANGUAGE_IMAGE_VERSION"
 else
-    DEFAULT_LANGUAGE_TAG="$(cat "$SCRIPT_DIR/../../language-image-version")"
+    DEFAULT_LANGUAGE_TAG="$(cat "$CONSTANTS_DIR/../../language-image-version")"
 fi
 export DEFAULT_LANGUAGE_TAG
 
 if [ -n "${SERVICE_IMAGE_VERSION:+x}" ]; then
     DEFAULT_SERVICE_TAG="$SERVICE_IMAGE_VERSION"
 else
-    DEFAULT_SERVICE_TAG="$(cargo metadata --no-deps --format-version 1 --manifest-path "$SCRIPT_DIR/../../Cargo.toml" | jq -r '.packages[] | select(.name == "proxy") | .version')"
+    DEFAULT_SERVICE_TAG="$(cargo metadata --no-deps --format-version 1 --manifest-path "$CONSTANTS_DIR/../../Cargo.toml" | jq -r '.packages[] | select(.name == "proxy") | .version')"
 fi
 export DEFAULT_SERVICE_TAG
 
